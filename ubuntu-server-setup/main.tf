d01c5fef389611cec8901a061f79feb8f610e3eb
shivajee98 <ultimateintimater@gmail.com>
2025-07-10 16:41:30 +0530

    first commit

---
+++ ubuntu-server-setup/main.tf
@@ -0,0 +1,178 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+# resource "aws_subnet" "subnet-1" {
+#   vpc_id     = aws_vpc.first-vpc.id
+#   cidr_block = "10.0.1.0/24"
+
+#   tags = {
+#     Name = "prod-subnet"
+#   }
+# }
+
+# resource "aws_vpc" "first-vpc" {
+#   cidr_block = "10.0.0.0/16"
+#   tags = {
+#     Name = "production"
+#   }
+# }
+
+
+
+
+# resource "aws_instance" "ec2-server" {
+#   ami = "ami-0f918f7e67a3323f0"
+#   instance_type = "t2.micro"
+
+#   tags = {
+#     Name = "ubuntu-1"
+#   }
+# }
+
+# 1. Create vpc
+
+resource "aws_vpc" "prod-vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "production"
+  }
+}
+
+# 2. Create Internet Gateway
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.prod-vpc.id
+
+  tags = {
+    Name = "main"
+  }
+}
+
+# 3. Create Custom Route Table
+
+resource "aws_route_table" "prod-route-table" {
+  vpc_id = aws_vpc.prod-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  route {
+    ipv6_cidr_block        = "::/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "prod"
+  }
+}
+
+
+# 4. Create a Subnet
+
+resource "aws_subnet" "subnet-1" {
+  vpc_id            = aws_vpc.prod-vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "ap-south-1a"
+  tags = {
+    Name = "prod-subnet"
+  }
+}
+
+# 5. Associate with Route Table
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.subnet-1.id
+  route_table_id = aws_route_table.prod-route-table.id
+}
+
+# 6. Create Security Group to allow port 22, 80, 443
+
+resource "aws_security_group" "allow_web" {
+  name        = "allow_web_traffic"
+  description = "Allow Web inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.prod-vpc.id
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_https_ipv4" {
+  security_group_id = aws_security_group.allow_web.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http_ipv4" {
+  security_group_id = aws_security_group.allow_web.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4" {
+  security_group_id = aws_security_group.allow_web.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_WEB_ipv4" {
+  security_group_id = aws_security_group.allow_web.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+
+
+# 7. Create Network interface with an ip in the subnet that was created in step 4
+
+resource "aws_network_interface" "web-server-nic" {
+  subnet_id       = aws_subnet.subnet-1.id
+  private_ips     = ["10.0.1.50"]
+  security_groups = [aws_security_group.allow_web.id]
+}
+
+
+# 8. Assign an elastic ip to the Network Interface created in step 7
+
+resource "aws_eip" "one" {
+  domain                    = "vpc"
+  network_interface         = aws_network_interface.web-server-nic.id
+  associate_with_private_ip = "10.0.1.50"
+  depends_on                = [aws_internet_gateway.gw]
+}
+
+
+# 9. Create Ubuntu server and install/enable apache2
+
+resource "aws_instance" "web-server-instance" {
+  ami               = "ami-0f918f7e67a3323f0"
+  instance_type     = "t2.micro"
+  availability_zone = "ap-south-1a"
+  key_name          = "key-name"
+  network_interface {
+    device_index         = 0
+    network_interface_id = aws_network_interface.web-server-nic.id
+  }
+  user_data = <<-EOF
+                #!/bin/bash
+                sudo apt update -y
+                sudo apt install apache2 -y
+                sudo systemctl start apache2
+                echo "your very first web server" > /var/www/html/index.html
+                EOF  
+  tags = {
+    Name = "Web-Server"
+  }
+}
