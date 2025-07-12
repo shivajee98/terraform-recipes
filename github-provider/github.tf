9f4858c8d4b74c4bc65377efb56010225bd2a300
shivajee98 <ultimateintimater@gmail.com>
2025-07-12 11:16:00 +0530

    remove github pat

--- github-provider/github.tf
+++ github-provider/github.tf
@@ -9,7 +9,7 @@ terraform {
 
 # Configure the GitHub Provider
 provider "github" {
-    token = "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"
+    token = "your github pat"
 }
 
 
