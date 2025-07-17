20e31f00edb5082a4f9cab655d58cc294cf86f39
shivajee98 <ultimateintimater@gmail.com>
2025-07-17 09:40:51 +0530

    cross-ref-attribute

--- terraform-cross-ref/cross-ref-attributes.tf
+++ terraform-cross-ref/cross-ref-attributes.tf
@@ -7,13 +7,56 @@ resource "aws_eip" "lb" {
 }
 
 resource "aws_security_group" "allow_tls" {
-  name = "attribute firewall"
+  name = "attribute-sg"
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
   security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = "${aws_eip.lb.public_ip}/32"
+  cidr_ipv4         = "${aws_eip.lb.public_ip}/32"                  # String interpolation
   from_port         = 80
   ip_protocol       = "tcp"
   to_port           = 80
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# provider "aws" {
+#   region = "us-east-1"
+# }
+
+# resource "aws_eip" "lb" {
+#   domain = "vpc"
+# }
+
+# resource "aws_security_group" "allow_tls" {
+#   name = "attribute firewall"
+# }
+
+# resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+#   security_group_id = aws_security_group.allow_tls.id
+#   cidr_ipv4         = "${aws_eip.lb.public_ip}/32"
+#   from_port         = 80
+#   ip_protocol       = "tcp"
+#   to_port           = 80
+# }
